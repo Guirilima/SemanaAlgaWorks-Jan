6c7bb4b79f236b81b117eaadff9b1287777ae600
Guirilima <ungui@outlook.com.br>
2021-01-14 00:03:34 -0300

    Add aula 2

---
+++ aula-2/algaworks-api/src/main/java/br/com/algaworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,66 @@
+package br.com.algaworks.api.exceptionhandler;
+
+import br.com.algaworks.domain.exception.NegocioException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/**
+ * Extends de ResponseEntityExceptionHandler
+ * Afim de obter já tratamentos com vários outros 'Exceptions'
+ * e logo abaixo poderemos reescreve-lo.
+ */
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Autowired
+	private MessageSource messageSource; //Afim de obter mensagem personalizada no messages.properties
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.BAD_REQUEST;
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDataHora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+		var campos = new ArrayList<Problema.Campo>();
+		
+		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+			String nome = ((FieldError) error).getField();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+			
+			campos.add(new Problema.Campo(nome, mensagem));
+		}
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Um ou mais campos estão inválidos. "
+				+ "Faça o preenchimento correto e tente novamente");
+		problema.setDataHora(LocalDateTime.now());
+		problema.setCampos(campos);
+		
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+	}
+	
+}
